0bbedcd3bb8cb84f12b40369c6594489ec63b113
Parth Sharma <sharmaaparth.07@gmail.com>
2025-12-20 19:27:31 +0530

    email_ops.py

--- email_ops.py
+++ email_ops.py
@@ -1,1 +1,23 @@
-#file created 
+from email.message import EmailMessage
+from smtplib import SMTP
+
+msg = EmailMessage()
+
+msg["From"] = "sender's gmail"
+msg["To"] = "receiver's gmail"
+msg["Subject"] = "your subject"
+
+msg.set_content("your content")
+
+try:
+    smtp = SMTP("smtp.gmail.com", 587)
+
+    smtp.starttls()
+    smtp.login("your gmail", 'your password')
+    smtp.send_message(msg)
+    smtp.quit()
+    print("SMTP connected sucessfully")
+
+
+except Exception as e:
+    print(f"Error: {e}")
